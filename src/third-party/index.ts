637067c0ebe39d93b67482b8ff41e9e2b97be28c
pnodet <paul.nodet@gmail.com>
2023-11-03 19:01:35 +0700

    chore: update build settings

---
+++ src/third-party/index.ts
@@ -0,0 +1,5 @@
+export * from './code';
+export * from './collection';
+export * from './equation';
+export * from './modal';
+export * from './pdf';
--- tsup.config.ts
+++ tsup.config.ts
@@ -3,11 +3,6 @@ import { Options, defineConfig } from 'tsup'
 const baseConfig: Options = {
   entry: [
     'src/index.tsx',
-    'src/third-party/code.tsx',
-    'src/third-party/collection.tsx',
-    'src/third-party/equation.tsx',
-    'src/third-party/modal.tsx',
-    'src/third-party/pdf.tsx'
   ],
   outDir: 'build',
   target: 'es2015',
@@ -21,14 +16,8 @@ const baseConfig: Options = {
 export default defineConfig([
   {
     ...baseConfig,
-    outDir: 'build/dev',
-    minify: false,
-    sourcemap: true
-  },
-  {
-    ...baseConfig,
     outDir: 'build',
-    minify: false,
-    sourcemap: false
+    minify: true,
+    sourcemap: true
   }
 ])
